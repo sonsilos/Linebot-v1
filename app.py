7d99e2f29d8d415f6c3c95beccabfce60ca8dbf2
t.thiticate <t.thiticate@unesco.org>
2018-11-06 17:17:54 +0700

    add job schedule

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import os, re, json
+import schedule
+import time
 from datetime import datetime, date, timedelta
 from flask import Flask, request, abort
 import requests
@@ -20,8 +22,19 @@ channel_access_token = os.getenv('LINE_CHANNEL_ACCESS_TOKEN', None)
 line_bot_api = LineBotApi(channel_access_token)
 handler = WebhookHandler(channel_secret)
 
+def job(t):
+    text_message = TextSendMessage(text='Hello, world')
+    line_bot_api.push_message('U3f3b79342293017ebce23e9bc12f5c63', text_message)
+    return
+
 text_message = TextSendMessage(text='Hello, world')
 line_bot_api.push_message('U3f3b79342293017ebce23e9bc12f5c63', text_message)
+
+schedule.every().day.at("17:00").do(job)
+
+while True:
+    schedule.run_pending()
+    time.sleep(60) # wait one minute
 
 @app.route('/')
 def homepage():
